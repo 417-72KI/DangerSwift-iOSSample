c6f96bee1362de74deb1ec227ef0cec0a95cddb0
417-72KI <417.72ki@gmail.com>
2021-04-22 00:22:00 +0900

    create new project

---
+++ DangerSwift-iOSSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DangerSwift-iOSSample
+//
+//  Created by 417.72KI on 2021/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
